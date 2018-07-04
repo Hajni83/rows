2cfcd1be6187dd9cf2af4a4cc0e0456accfc1b00
József Tar <szurilo@gmail.com>
2018-07-04 22:49:08 +0200

    fine

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ $(function() {
     var canvas=document.getElementById('MyCanvas');
     var context=canvas.getContext('2d');
     context.fillStyle='orange';
-    context.strokeStyle = "black";
-    for(i=0;i<5;i++){
-        console.log('i:'+i);
-        for(j=0;j<6;j++){
-            console.log('j:'+j);
-            context.fillRect(i*50,50,50,50);
-            context.strokeRect(i*50,50,50,50);
+    context.strokeStyle = "white";
+    for(i=0; i<5; i++) {
+        for(j=0; j<i+1; j++) {
+            context.fillRect(50 + j * 50, 50 + i * 50, 45, 45);
+            //context.strokeRect(50 + j * 50, 50 + i * 50, 50, 50);
         }
     }
   });
